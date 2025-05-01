ced28b721582f7df04c24b24ab8604f620b4a674
fxckingtom <uyu99876a@gmail.com>
2025-05-01 21:21:33 +0800

    Update frontend code before deployment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-<Router>
+<Router basename="/health-info-frontend-new">
   <Navbar />
   <Routes>
     <Route path="/" element={<DiseaseList />} />
