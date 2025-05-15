4e26fe062621e89cde711e3fdec6cd503e7d0fa2
fxckingtom <uyu99876a@gmail.com>
2025-05-15 12:24:44 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import HealthInfo from './components/HealthInfo';
 import HealthLog  from './components/HealthLog';
 import HealthyRecipes from './HealthyRecipes';
 
+/**
+ * 关键：加上 basename，让所有内部跳转都以 /health-info-frontend-new 为根
+ */
 function App() {
   return (
-    <Router>
+    <Router basename="/health-info-frontend-new">
       <Navbar />
       <Routes>
-        <Route path="/"             element={<DiseaseList />} />
-        <Route path="/health-info"  element={<HealthInfo />} />
-        <Route path="/health-log"   element={<HealthLog />} />
+        <Route path="/"               element={<DiseaseList   />} />
+        <Route path="/health-info"    element={<HealthInfo    />} />
+        <Route path="/health-log"     element={<HealthLog     />} />
         <Route path="/healthy-recipes" element={<HealthyRecipes />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
+
 
 
 
