ab8fd0348f2ba17e13dc2e6a146316f25c89a6c8
fxckingtom <uyu99876a@gmail.com>
2025-05-15 11:12:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-// import { BrowserRouter as Router, ... } 
+// src/App.js
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import DiseaseList from './components/DiseaseList';
-import HealthInfo from './components/HealthInfo';
-import HealthLog from './components/HealthLog';
-import Navbar from './components/Navbar';
-import HealthyRecipes from './HealthyRecipes';
+import HealthInfo   from './components/HealthInfo';
+import HealthLog    from './components/HealthLog';
+import Navbar       from './components/Navbar';
+import HealthyRecipes from './components/HealthyRecipes';
 
 function App() {
   return (
-<Router>
-  <Navbar />
-  <Routes>
-    <Route path="/" element={<DiseaseList />} />
-    <Route path="/health-info" element={<HealthInfo />} />
-    <Route path="/health-log" element={<HealthLog />} />
-    <Route path="/healthy-recipes" element={<HealthyRecipes />} />
-  </Routes>
-</Router>
+    <Router basename="/health-info-frontend-new">
+      <Navbar />
+      <Routes>
+        <Route path="/"            element={<DiseaseList />} />
+        <Route path="/health-info" element={<HealthInfo   />} />
+        <Route path="/health-log"  element={<HealthLog    />} />
+        <Route path="/healthy-recipes" element={<HealthyRecipes />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
