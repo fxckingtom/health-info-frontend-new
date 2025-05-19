bee21b8e73c60c3fb7c4ffd606ab139e157ace10
Anderson0520 <andersonlo20050520@gmail.com>
2025-05-19 19:02:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import DiseaseList from './components/DiseaseList';
 import HealthInfo   from './components/HealthInfo';
 import HealthLog    from './components/HealthLog';
