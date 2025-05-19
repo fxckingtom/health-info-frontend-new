40d6b120285eb285a37721ecb5e729e4b453316b
Anderson0520 <andersonlo20050520@gmail.com>
2025-05-19 14:39:32 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-primary text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold">健康資訊網</Link>
-        <div className="space-x-5">
+        <div className="space-x-4">
           <Link to="/" className="hover:underline">首頁</Link>
           <Link to="/health-info" className="hover:underline">健康資訊</Link>
           <Link to="/health-log" className="hover:underline">健康日誌</Link>
