eea4f317c457bfcb094f2e4a0ee2e388af795ea2
Anderson0520 <andersonlo20050520@gmail.com>
2025-05-19 14:40:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import MapComponent from './components/MapComponent';
 
 function App() {
   return (
-    <Router basename="/health-info-frontend-new">
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/"            element={<DiseaseList />} />
