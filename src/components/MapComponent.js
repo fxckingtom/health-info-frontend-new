184a34e289ba15e9ccbc6e66b973e461bdeda09e
Anderson0520 <andersonlo20050520@gmail.com>
2025-05-22 11:53:21 +0800

    Update MapComponent.js

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -8,13 +8,12 @@ import '../MapComponent.css';
 
 // 自訂 Marker 圖示
 const blueIcon = new L.Icon({
-  iconUrl: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/images/marker-icon-2x-blue.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
+  iconUrl: 'https://chart.googleapis.com/chart?chst=d_map_pin_icon&chld=home|00f',
+  iconSize: [30, 50],
+  iconAnchor: [15, 50],
+  popupAnchor: [0, -40],
 });
+
 
 const redIcon = new L.Icon({
   iconUrl: 'https://cdn.jsdelivr.net/npm/leaflet@1.9.3/dist/images/marker-icon-2x.png',
