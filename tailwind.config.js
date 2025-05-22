a5f0c6d5a2bfe15d887e16d43529fef6711a956f
fxckingtom <uyu99876a@gmail.com>
2025-05-22 13:01:20 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    container: {
+      center: true,          // 自动水平居中
+      padding: {
+        DEFAULT: '1rem',     // 默认左右 1rem
+        sm:      '2rem',     // sm 断点以上 2rem
+        lg:      '4rem',     // lg 以上 4rem
+        xl:      '5rem',
+        '2xl':   '6rem',
+      },
+    },
     extend: {
       colors: {
         primary: {
