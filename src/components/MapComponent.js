20e4adc3d59e8924ebc9f1ad57f4b6f52d6e5f82
Anderson0520 <andersonlo20050520@gmail.com>
2025-05-22 11:45:32 +0800

    Update MapComponent.js

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import './MapComponent.css';
+import '../MapComponent.css';
 
 // 自訂 Marker 圖示
 const blueIcon = new L.Icon({
